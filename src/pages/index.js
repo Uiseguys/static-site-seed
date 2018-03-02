9699328641f7003d877bb2d7057500530d38abbb
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-03-02 12:49:39 +0200

    Add 2 pages. Fixes #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+export default () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi!</h1>
+    <p>
+      <Link to='/dashboard/'>Dashboard</Link>
+    </p>
+    <p>
+      <Link to='/about/'>About</Link>
+    </p>
   </div>
 )
-
-export default IndexPage
